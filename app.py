a7bd72070a578a4bcfc827631f520de5885b68ff
Thắm Tạo_KTM <213826594+ThamKTM155@users.noreply.github.com>
2026-05-10 20:57:40 +0700

    Update app.py
    Cập nhật lại ```python id="v6m2pk"
    model = genai.GenerativeModel("models/gemini-1.5-flash")
    ```

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ CORS(app)
 genai.configure(
     api_key=os.getenv("GEMINI_API_KEY")
 )
-genai.GenerativeModel("models/gemini-1.5-flash")
+model = genai.GenerativeModel("models/gemini-1.5-flash")
 @app.route("/")
 def home():
     return {
