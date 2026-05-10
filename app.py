71391e400b4d5df3b71461e7bbc44547fd5758de
Thắm Tạo_KTM <213826594+ThamKTM155@users.noreply.github.com>
2026-05-10 19:29:05 +0700

    Update model version to 'gemini-1.5-flash-latest'
    Sửa nội dung app.py

--- app.py
+++ app.py
@@ -10,9 +10,7 @@ CORS(app)
 genai.configure(
     api_key=os.getenv("GEMINI_API_KEY")
 )
-
-model = genai.GenerativeModel("gemini-1.5-flash")
-
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 @app.route("/")
 def home():
     return {
